77db89eea8e3cc76c9faecd069d97de5f9761ca6
Aliyawan Aris <aliyawan129@gmail.com>
2024-12-24 20:57:27 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// //window.alert("index.js");
+// const age = 10;
+// const name = 'Dicoding';
+// const pi = 'Dia berkata, "Halo!"';
+
+// console.log(`Aku ${name}, umurku ${age} tahun.`);
+// console.log(pi);
+
+// /*
+// Komentar
+// */
+
+// let username = 'Dicoding';
+
+// console.log('Sebelum diubah:', username); // output: Sebelum diubah: Dicoding
+// username = 'dicodingacademy';
+// console.log('Setelah diubah:', username); // output: Setelah diubah: dicodingacademy
+
+// function companyName(){
+//     const name = 'Artazama';
+//     const status = 'active';
+
+//     console.log('Company', name);
+//     console.log('Status', status);
+// }
+
+// function companyName(){
+//     const name = 'Aris';
+//     const age = 'Productive';
+
+//     console.log('Company', name);
+//     console.log('Status', age);
+// }
+
+const currentYear = new Date().getFullYear();
+const text = `Sekarang adalah tahun ${currentYear}.`;
+
+console.log(text);
+
+const result = 50 / 0;
+console.log(result); // output: Infinity
+
+const completed = true;
+const passed = false;
+let draw = true;
+
+console.log(completed, passed, draw); // output: true false
+
+const isGreater2 = 5 > 12;
+
+console.log(isGreater2); // output: true (5 lebih besar dari 2)
